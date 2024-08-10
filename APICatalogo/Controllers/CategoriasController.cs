6ba1d9bf729d3ef20a846fc56c4852f0dfabb6b5
Pedro Bezerra <pcbobezerra@gmail.com>
2024-08-10 09:57:13 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,102 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            try
+            {
+                return _context.Categorias.AsNoTracking().ToList();
+            }
+            catch (Exception) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um Problema ao tratar a sua solicitação.");
+            }
+        }
+
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            try
+            {
+                return _context.Categorias.Include(p => p.Produtos).AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um Problema ao tratar a sua solicitação.");
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategorias")]
+        public ActionResult Get(int id)
+        {
+       
+            try
+            {
+                var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+                if (categoria is null)
+                { return NotFound(); }
+                return Ok(categoria);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um Problema ao tratar a sua solicitação.");
+            }
+
+
+        }
+
+        [HttpPost("{id:int}")]
+        public ActionResult Post(int id, Categoria categoria)
+        {
+            if (categoria is null)
+                return BadRequest();
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId) { return BadRequest(); }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(categoria);
+
+        }
+
+        [HttpDelete ("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound();
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20240717183214_PopulaProdutos.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaID)" + 
+                "Values('Coca-Cola','Refrigerante de Cola 350ml',5.45,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaID)" +
+                "Values('Misto Quente','Pão Francês, queijo,presunto,tomate.',19.00,'mistoquente.jpg',100,now(),2)");
+
+            mb.Sql("insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaID)" +
+                "Values('Sorvete de Creme','um delicioso sorvete de creme',5.00,'sorvetecreme.jpg',40,now(),3)");
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        { 
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
